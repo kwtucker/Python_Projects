27f0048778ae8735c0b68c301486934871d6e248
Kevin Tucker <kwtucker4@gmail.com>
2016-03-04 16:09:15 -0500

    set up the database columns

--- day1/PyEnvironments/devent/src/posts/admin.py
+++ day1/PyEnvironments/devent/src/posts/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['group_name','event_name','event_date','event_id','event_url', 'comment']
     class Meta:
         model = Post
 admin.site.register(Post, PostModelAdmin)
