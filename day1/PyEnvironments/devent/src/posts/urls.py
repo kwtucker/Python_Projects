0e244b2198989321f0ac67844441b77c84a50238
Kevin Tucker <kwtucker4@gmail.com>
2016-03-04 16:13:19 -0500

    Updated the urls with the get and post_comment

--- day1/PyEnvironments/devent/src/posts/urls.py
+++ day1/PyEnvironments/devent/src/posts/urls.py
@@ -3,16 +3,18 @@ from django.contrib import admin
 from .views import (
                     post_list,
                     post_create,
-                    post_detail,
+                    # post_detail,
                     post_update,
                     post_delete,
                     get,
+                    post_comment,
                     )
 urlpatterns = [
     url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
-    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    # url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     url(r'^(?P<id>\d+)/delete/$', post_delete),
-    url(r'^get/$', get),
+    url(r'^get/$', get, name='get'),
+    url(r'^edit/(?P<id>\d+)/$', post_comment, name='comment'),
 ]
