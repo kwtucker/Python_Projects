dfd98159f5d43fdab270d430ab099924865f9e14
Kevin Tucker <kwtucker4@gmail.com>
2016-03-06 23:20:00 -0500

    Added comments

--- Django_Meetup_app/PyEnvironments/devent/src/static/js/main.js
+++ Django_Meetup_app/PyEnvironments/devent/src/static/js/main.js
@@ -1,5 +1,7 @@
 
 $(function() {
+    // This is for the comment button on the Your list View.
+    // It will show and hide the comment and also change the text of the button
     $('.btn').addClass('comShow')
     $('.comShow').click(function(e) {
         e.preventDefault();
@@ -12,6 +14,9 @@ $(function() {
         }
     });
 
+    // This converts the epoch time stamp to a human readable format
+    // Takes the el object and turns it to a string then a number.
+    // Sets it to a date and turns the timestamp to human readable.
     $('.datetime small').each(function(index, el) {
         JSON.stringify(el)
         var date = Number(el.innerHTML)
