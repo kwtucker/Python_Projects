47094e7f68c594073f99774b09dfa8d45825f8a5
Kevin Tucker <kwtucker4@gmail.com>
2016-03-15 22:06:41 -0400

    Keys added to tables

--- git_app/models.py
+++ git_app/models.py
@@ -1,41 +1,5 @@
 from app import db
 
-
-# class DevPost(db.Model):
-#     __tablename__ = "posts"
-
-#     id      = db.Column(db.Integer, primary_key=True)
-#     title   = db.Column(db.String(120), nullable=False)
-#     author  = db.Column(db.String(120), nullable=False)
-#     content = db.Column(db.Text, nullable=False)
-
-#     def __init__(self, title, author, content):
-#         self.title   = title
-#         self.author  = author
-#         self.content = content
-
-#     def __repr__(self):
-#         return '{}'.format(self.title)
-
-
-# class Users(db.Model):
-#     __tablename__ = "users"
-
-#     id            = db.Column(db.Integer, primary_key=True)
-#     user_name     = db.Column(db.String(120), nullable=True)
-#     user_email    = db.Column(db.String(120), nullable=True)
-#     user_password = db.Column(db.String(120), nullable=True)
-#     github_access_token   = db.Column(db.String(255), nullable=True)
-
-#     def __init__(self, user_name, user_email, user_password, github_access_token):
-#         self.user_name     = user_name
-#         self.user_email    = user_email
-#         self.user_password = user_password
-#         self.github_access_token   = github_access_token
-
-#     def __repr__(self):
-#         return '{}'.format(self.user_name)
-
 class Users(db.Model):
     __tablename__ = "Users"
 
@@ -45,7 +9,6 @@ class Users(db.Model):
 
 
     def __init__(self, github_access_token):
-        # self.user_id               = the_user_id
         self.github_access_token   = github_access_token
 
     def __repr__(self):
@@ -61,7 +24,6 @@ class Settings(db.Model):
 
 
     def __init__(self, name):
-        # self.set_id = set_id
         self.name   = name
 
     def __repr__(self):
