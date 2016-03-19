dd7c59622e6e641c5022ba366387e11dd1fe1422
Kevin Tucker <kwtucker4@gmail.com>
2016-03-19 16:52:57 -0400

    update

--- git_app/git_add.py
+++ git_app/git_add.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 def git():
-    import subprocess
-    subprocess.Popen(['cd','../'])
-    ls  = subprocess.Popen(['ls','-l'], stdout=subprocess.PIPE)
-    awk = subprocess.Popen(["awk '{{print $9}}'"], stdin=ls.stdout,shell=True)
-    ls.wait()
-    stdout = awk.communicate()[0]
+    import subprocess, os
+    repos =  os.listdir("../")
+
+    for r in repos:
+        if '.' not in r:
+            subprocess.Popen(['cd ../',r])
+            i = subprocess.Popen(['git','status'])
+            print i.stdout
+            subprocess.Popen(['cd','../scripts'])
+
+
+
+    # subprocess.Popen(['cd','../'])
+
+    # ls  = subprocess.Popen(['ls','-l'], stdout=subprocess.PIPE)
+
+    # awk = subprocess.Popen(["awk '{{print $9}}'"], stdin=ls.stdout,shell=True)
+    # ls.wait()
+    # stdout = ls.communicate()[0]
+
     # print stdout
-    print "Git Add Commit"
+    # print "Git Add Commit"
 
 
 # proc = subprocess.Popen(["virsh dumpxml {0} | grep 'source file' | awk -F\\' '{{print $2}}'".format(vm)], stdout=subprocess.PIPE, shell=True)
